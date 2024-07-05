5a4e3bc99a7a135f3968e225a066d2fbf71e0cde
TresaAnnThomas <teeesa728@gmail.com>
2024-07-05 20:49:37 +0800

    Create Wakanda flag using Shaders

--- L27/src/script.js
+++ L27/src/script.js
@@ -20,6 +20,7 @@ const scene = new THREE.Scene()
  * Textures
  */
 const textureLoader = new THREE.TextureLoader()
+const flagTexture = textureLoader.load('/textures/flag.png')
 
 /**
  * Test mesh
@@ -29,14 +30,39 @@ const geometry = new THREE.PlaneGeometry(1, 1, 32, 32)
 
 const count = geometry.attributes.position.count
 
+const randoms = new Float32Array(count)
+for (let i = 0; i < count; i++) {
+    randoms[i] = Math.random()
+}
+geometry.setAttribute('aRandom', new THREE.BufferAttribute(randoms, 1))
+
 // Material
-const material = new THREE.RawShaderMaterial({
+const material = new THREE.ShaderMaterial({
     vertexShader: testVertexShader,
-    fragmentShader: testFragmentShader
+    fragmentShader: testFragmentShader,
+    uniforms: {
+        uFrequency: {
+            value: new THREE.Vector2(10.0, 5.0)
+        },
+        uTime: {
+            value: 0
+        },
+        uColor: {
+            value: new THREE.Color('orange')
+        },
+        uTexture: {
+            value: flagTexture
+        }
+    }
 })
+
+gui.add(material.uniforms.uFrequency.value, 'x').min(1).max(10).step(0.01).name('frequencyX')
+gui.add(material.uniforms.uFrequency.value, 'y').min(1).max(10).step(0.01).name('frequencyY')
+
 
 // Mesh
 const mesh = new THREE.Mesh(geometry, material)
+mesh.scale.y = 0.75
 scene.add(mesh)
 
 /**
@@ -91,6 +117,9 @@ const clock = new THREE.Clock()
 const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
+
+    //Update material
+    material.uniforms.uTime.value = elapsedTime
 
     // Update controls
     controls.update()
--- L27/src/shaders/test/vertex.glsl
+++ L27/src/shaders/test/vertex.glsl
@@ -1,18 +1,33 @@
- uniform mat4 projectionMatrix;
-        uniform mat4 viewMatrix;
-        uniform mat4 modelMatrix;
 
-        attribute vec3 position;
+        uniform vec2 uFrequency;
+        uniform float uTime;
+
+        attribute float aRandom;
+ 
+
+        varying vec2 vUv;
+        varying float vElevation;
+
 
         void main()
         {
             vec4 modelPosition = modelMatrix * vec4(position, 1.0);
 
-            modelPosition.z += sin(modelPosition.x * 10.0) * 0.1;
+            float elevation = sin(modelPosition.x * uFrequency.x - uTime) * 0.1;
+            elevation += sin(modelPosition.x * uFrequency.y - uTime) * 0.1;
+
+            modelPosition.z += elevation;
+
+           //modelPosition.z += aRandom * 0.1;
+          
 
             vec4 viewPosition = viewMatrix * modelPosition;
             vec4 projectedPosition = projectionMatrix * viewPosition;
 
-            gl_Position = projectedPosition;
+            gl_Position = projectedPosition; //position of vertex on the render
             //gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(position, 1.0);
+
+            vUv = uv;
+            vElevation = elevation;
+    
         }
